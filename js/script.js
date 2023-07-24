8715bd8a868beb7d0549c0ef19c83d6b6c4296bd
Artur Feliks <arturfeliks4@gmail.com>
2023-07-24 12:38:16 +0200

    remove tasks button added

--- js/script.js
+++ js/script.js
@@ -2,19 +2,10 @@
     const tasks = [
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for(const task of tasks) {
-            htmlString += `
-            <li>
-            ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -24,14 +15,37 @@
         render();
     };
 
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            <button class="js-remove">Usuń</button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-    if (newTaskContent === "") {
-        return;
-    }
+        if (newTaskContent === "") {
+            return;
+        }
         addNewTask(newTaskContent);
     };
 
